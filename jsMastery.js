fd709cf547b5728f9aaff2e06eefef1494eacfc7
Wladyslaw Ko <ever.at.work@gmail.com>
2020-11-24 21:56:37 +0200

    доработол жыэс

--- jsMastery.js
+++ jsMastery.js
@@ -1,11 +1,12 @@
 // The New Year Countdown JS part 
 
-const days = document.getElementById('days');
-const hours = document.getElementById('hours');
-const minutes = document.getElementById('minutes');
-const seconds = document.getElementById('seconds');
-const countdown = document.getElementById('countdown');
+const days = document.querySelector('#days');
+const hours = document.querySelector('#hours');
+const minutes = document.querySelector('#minutes');
+const seconds = document.querySelector('#seconds');
+const countdown = document.querySelector('#countdown');
 
+const year = document.querySelector('#year');
 
 
 const currentYear = new Date().getFullYear();
@@ -21,10 +22,15 @@ function updateCountdown() {
     const m = Math.floor(diff / 1000 / 60) % 60;
     const s = Math.floor(diff / 1000) % 60;
 
+    // добавить значения в DOM
     days.innerHTML = d;
     hours.innerHTML = h < 10 ? '0' + h : h; // дабы отображался нулик перед значением меньше 10
     minutes.innerHTML = m < 10 ? '0' + m : m;
     seconds.innerHTML = s < 10 ? '0' + s : s;
 }
+
+// отображать спинор :)
+
+// Запускается ежесекундно 
 setInterval(updateCountdown, 1000);
-updateCountdown();
+
