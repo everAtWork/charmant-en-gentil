08fc04388bfaccdc8d4c1b00b15b04d6ed42f591
Wladyslaw Ko <ever.at.work@gmail.com>
2020-11-24 21:58:22 +0200

    не все коммитнулось сцкс

--- jsMastery.js
+++ jsMastery.js
@@ -7,12 +7,14 @@ const seconds = document.querySelector('#seconds');
 const countdown = document.querySelector('#countdown');
 
 const year = document.querySelector('#year');
+const loading = document.querySelector('#loading');
 
-
+// Set background year
+year.innerText = currentYear + 1;
 const currentYear = new Date().getFullYear();
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 
-
+// Времени обновлятор
 function updateCountdown() {
     const currentTime = new Date();
     const diff = newYearTime - currentTime;
@@ -29,7 +31,12 @@ function updateCountdown() {
     seconds.innerHTML = s < 10 ? '0' + s : s;
 }
 
-// отображать спинор :)
+// отображать спинор перед появлением счётчика :)
+setTimeout(() => {
+    loading.remove();
+    countdown.style.display = 'flex';
+
+}, 1000);
 
 // Запускается ежесекундно 
 setInterval(updateCountdown, 1000);
